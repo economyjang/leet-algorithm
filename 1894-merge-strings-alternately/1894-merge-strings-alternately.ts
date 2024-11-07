968c18edd3b63284ff986bc3f444aaf69c3dee90
Kyungjae Jang <economyjang777@gmail.com>
2024-11-07 23:34:03 +0900

    Time: 63 ms (37.23%), Space: 52.1 MB (51.56%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.ts
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.ts
@@ -1,12 +1,13 @@
 function mergeAlternately(word1: string, word2: string): string {
     let answer = ''
 
-    let word1Arr = word1.split('')
-    let word2Arr = word2.split('')
+    let word1Pointer = 0
+    let word2Pointer = 0
 
-    while(word1Arr.length || word2Arr.length) {
-        if(word1Arr.length) answer += word1Arr.shift()
-        if(word2Arr.length) answer += word2Arr.shift()
+    while(true) {
+        if(word1Pointer !== word1.length) answer += word1[word1Pointer++]
+        if(word2Pointer !== word2.length) answer += word2[word2Pointer++]
+        if(word1Pointer + word2Pointer === word1.length + word2.length) break
     }
 
     return answer
