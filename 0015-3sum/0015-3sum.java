84899cabcb34c62e54193d5223247905afc98d8d
Kyungjae Jang <economyjang777@gmail.com>
2025-06-25 20:16:34 +0900

    Time: 30 ms (69.47%), Space: 52.5 MB (22.37%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,31 +1,31 @@
 import java.util.*;
 
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        List outerList = new ArrayList<ArrayList<Integer>>();
+    public List<List<Integer>> threeSum(int[] arr) {
+        List<List<Integer>> outerList = new ArrayList<>();
 
-        Arrays.sort(nums);
+        Arrays.sort(arr);
 
-        for(int i = 0; i < nums.length; i++) {
-            if(i > 0 && nums[i - 1] == nums[i]) continue;
-            int target = nums[i];
-            int left = i + 1, right = nums.length - 1;
+        for(int i = 0; i < arr.length; i++){
+            if(i > 0 && arr[i] == arr[i - 1]) continue;
+            int target = arr[i];
+            int left = i + 1, right = arr.length - 1;
 
-            while(left < right) {
-                int sum = target + nums[left] + nums[right];
+            while(left < right){
+                int sum = target + arr[left] + arr[right];
 
                 if(sum == 0) {
-                    List innerList = new ArrayList<Integer>();
+                    List<Integer> innerList = new ArrayList<>();
                     innerList.add(target);
-                    innerList.add(nums[left]);
-                    innerList.add(nums[right]);
+                    innerList.add(arr[left]);
+                    innerList.add(arr[right]);
                     outerList.add(innerList);
-                    
+
+                    while(left < right && arr[left] == arr[left + 1]) left++;
+                    while(left < right && arr[right] == arr[right - 1]) right--;
+
                     left++;
                     right--;
-
-                    while(left < right && nums[left] == nums[left - 1]) left++;
-                    while(left < right && nums[right] == nums[right + 1]) right--;
                 } else if(sum < 0) {
                     left++;
                 } else {
