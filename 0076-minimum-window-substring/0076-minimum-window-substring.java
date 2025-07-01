3171b963db260323d1d2c6fae609175759bc21ca
Kyungjae Jang <economyjang777@gmail.com>
2025-07-01 23:25:39 +0900

    Time: 21 ms (36.67%), Space: 45.3 MB (49.74%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,40 +1,46 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int minWindow = Integer.MAX_VALUE;
-        int minStart = 0;
+        int minWindowSize = Integer.MAX_VALUE;
+        int minStartIdx = 0;
 
-        Map<Character, Integer> windowCharMap = new HashMap<>();
-        Map<Character, Integer> tCharMap = new HashMap<>();
+        Map<Character, Integer> subStrMap = new HashMap<>();
+        Map<Character, Integer> originStrMap = new HashMap<>();
+
         for(char ch : t.toCharArray()) {
-            tCharMap.put(ch, tCharMap.getOrDefault(ch, 0) + 1);
+            subStrMap.put(ch, subStrMap.getOrDefault(ch, 0) + 1);
         }
 
-        int left = 0, need = tCharMap.size(), have = 0;
-        for(int right = 0; right < s.length(); right++) {
-            char rightCh =  s.charAt(right);
-            windowCharMap.put(rightCh, windowCharMap.getOrDefault(rightCh, 0) + 1);
+        int left = 0, right = 0, need = subStrMap.size(), have = 0;
+        while(right < s.length()){
+            char rightChar = s.charAt(right);
+            originStrMap.put(rightChar, originStrMap.getOrDefault(rightChar, 0) + 1);
 
-            if(tCharMap.containsKey(s.charAt(right)) && tCharMap.get(s.charAt(right)).intValue() == windowCharMap.get(s.charAt(right)).intValue()) {
+            if(subStrMap.containsKey(rightChar) &&
+                    subStrMap.get(rightChar).intValue() == originStrMap.get(rightChar).intValue()) {
                 have++;
             }
 
-            while(have == need) {
-                if(minWindow > right - left + 1) {
-                    minWindow = right - left + 1;
-                    minStart = left;
+            while(need == have) {
+                if(minWindowSize > right - left + 1) {
+                    minWindowSize = right - left + 1;
+                    minStartIdx = left;
                 }
 
-                char leftCh =  s.charAt(left);
-                windowCharMap.put(leftCh, windowCharMap.get(leftCh) - 1);
+                char leftChar = s.charAt(left);
+                originStrMap.put(leftChar, originStrMap.get(leftChar) - 1);
 
-                if (tCharMap.containsKey(leftCh) && windowCharMap.get(leftCh).intValue() < tCharMap.get(leftCh).intValue()) {
+                if(subStrMap.containsKey(leftChar) &&
+                        originStrMap.get(leftChar) < subStrMap.get(leftChar)) {
                     have--;
                 }
 
+
                 left++;
             }
+
+            right++;
         }
 
-        return minWindow == Integer.MAX_VALUE ? "" : s.substring(minStart, minStart + minWindow);
+        return minWindowSize == Integer.MAX_VALUE ? "" : s.substring(minStartIdx, minStartIdx + minWindowSize);
     }
 }
