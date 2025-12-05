c7966479c3ba27a2b33d3c47b65ad723ab081e27
Kyungjae Jang <economyjang777@gmail.com>
2025-12-05 15:39:50 +0900

    Time: 23 ms (86%), Space: 19.9 MB (11.32%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -1,7 +1,4 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        nums.sort()
-        result = sum(nums[::2])
-
-        return result
+        return sum(sorted(nums)[::2])
         
