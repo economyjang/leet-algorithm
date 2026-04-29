019d77482ab19deacacb65b157ba825cc30c922f
Kyungjae Jang <economyjang777@gmail.com>
2026-04-29 14:46:07 +0900

    Time: 44 ms (26.64%), Space: 37.8 MB (97.94%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -11,11 +11,11 @@ class Solution:
         while fast and fast.next:
             fast = fast.next.next
             rev, rev.next, slow = slow, rev, slow.next
-
+        
         if fast:
             slow = slow.next
 
         while rev and rev.val == slow.val:
             rev, slow = rev.next, slow.next
-
+        
         return not rev
