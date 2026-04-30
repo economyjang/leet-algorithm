a3b0bc058d68cff4b4187ed4592daf607c5690b7
Kyungjae Jang <economyjang777@gmail.com>
2026-04-30 21:15:43 +0900

    Time: 0 ms (100%), Space: 19.1 MB (99.74%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,24 +1,27 @@
 class MyQueue:
-
     def __init__(self):
         self.input = []
         self.output = []
 
+    # 요소 x 를 큐 마지막에 삽입
     def push(self, x: int) -> None:
         self.input.append(x)
 
+    # 큐 처음에 있는 요소를 제거
     def pop(self) -> int:
         self.peek()
         return self.output.pop()
 
+    # 큐 처음에 있는 요소를 조회
     def peek(self) -> int:
         if not self.output:
             while self.input:
                 self.output.append(self.input.pop())
         return self.output[-1]
 
+    # 큐가 비어 있는지 여부를 리턴
     def empty(self) -> bool:
-        return not self.input and not self.output
+        return self.input == [] and self.output == []
 
 
 # Your MyQueue object will be instantiated and called as such:
