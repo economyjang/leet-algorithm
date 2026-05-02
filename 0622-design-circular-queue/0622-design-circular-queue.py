01e6dd144e3a5b3bceee0f3c5b84b954b3b21d1f
Kyungjae Jang <economyjang777@gmail.com>
2026-05-02 14:21:37 +0900

    Time: 8 ms (44.55%), Space: 19.9 MB (86.37%) - LeetHub

--- 0622-design-circular-queue/0622-design-circular-queue.py
+++ 0622-design-circular-queue/0622-design-circular-queue.py
@@ -1,40 +1,36 @@
 class MyCircularQueue:
     def __init__(self, k: int):
-        self.front = 0
-        self.rear = 0
-        self.length = k
-        self.q = [None] * self.length
+        self.q = [None] * k
+        self.maxlen = k
+        self.p1 = 0
+        self.p2 = 0
 
+    #   enQueue(): rear 포인터 이동
     def enQueue(self, value: int) -> bool:
-        if self.q[self.rear] is None:
-            self.q[self.rear] = value
-            self.rear = (self.rear + 1) % self.length
+        if self.q[self.p2] is None:
+            self.q[self.p2] = value
+            self.p2 = (self.p2 + 1) % self.maxlen
             return True
         else:
             return False
 
+    # deQueue(): front 포인터 이동
     def deQueue(self) -> bool:
-        if self.q[self.front] is None:
+        if self.q[self.p1] is None:
             return False
         else:
-            self.q[self.front] = None
-            self.front = (self.front + 1) % self.length
+            self.q[self.p1] = None
+            self.p1 = (self.p1 + 1) % self.maxlen
             return True
 
     def Front(self) -> int:
-        if self.q[self.front] is None:
-            return -1
-        else:
-            return self.q[self.front]
+        return -1 if self.q[self.p1] is None else self.q[self.p1]
 
     def Rear(self) -> int:
-        if self.q[self.rear - 1] is None:
-            return -1
-        else:
-            return self.q[self.rear - 1]
+        return -1 if self.q[self.p2 - 1] is None else self.q[self.p2 - 1]
 
     def isEmpty(self) -> bool:
-        return self.front == self.rear and self.q[self.front] is None
+        return self.p1 == self.p2 and self.q[self.p1] is None
 
     def isFull(self) -> bool:
-        return self.front == self.rear and self.q[self.rear] is not None
+        return self.p1 == self.p2 and self.q[self.p1] is not None
